4022bf0d868b4bd475df55245636b931beebb81e
WGonzalez <student@wgonzalez.local>
2017-01-13 22:14:20 -0600

    Initial Commit

---
+++ ProgressBar PROTOTYPE II/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProgressBar PROTOTYPE II
+//
+//  Created by WGonzalez on 1/13/17.
+//  Copyright © 2017 Quantum Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
