b937d8e67e85fa3c1024bd0c4c541a4aba7edbd9
nmalin-jones <student@nmalin-jones.hsd1.il.comcast.net>
2017-01-22 15:21:12 -0600

    lol

--- ProgressBar PROTOTYPE II/ViewController.swift
+++ ProgressBar PROTOTYPE II/ViewController.swift
@@ -34,9 +34,11 @@ class ViewController: UIViewController
     @IBOutlet weak var enemy: UIImageView!
     @IBOutlet weak var friendly: UIImageView!
     
+    //timers
     var timer : Timer?
     var turnTimer : Timer?
     
+    //variables
     var currentProgressFriend = 1.0
     var currentProgressEnemy = 1.0
     var FriendAttackPower = 0.0
@@ -55,26 +57,25 @@ class ViewController: UIViewController
         friendlyHealth.transform = friendlyHealth.transform.scaledBy(x: 1, y: 5)
         enemyHealth.transform = enemyHealth.transform.scaledBy(x: 1, y: 5)
         
+        //sets the progress of the progress view
         friendlyHealth.progress = 1
         enemyHealth.progress = 1
         
+        //sets the text of the labels
         labelOne.text = "3"
         labelTwo.text = "5"
         labelThree.text = "7"
         labelFour.text = "10"
         
-        buttonOne.backgroundColor = UIColor.lightGray
-        buttonTwo.backgroundColor = UIColor.lightGray
-        buttonThree.backgroundColor = UIColor.lightGray
-        buttonFour.backgroundColor = UIColor.lightGray
-        
         displayLabel.text = "\(currentCount)"
         
+        //can't tap the button
         buttonOne.isEnabled = false
         buttonTwo.isEnabled = false
         buttonThree.isEnabled = false
         buttonFour.isEnabled = false
         
+        //timer that allows the progress bar to move and keep track on the game
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(moveTheProgressBar), userInfo: nil, repeats: true)
        
     }
@@ -86,85 +87,137 @@ class ViewController: UIViewController
         {
             limit += 1.0
             limitTwo += 1.0
+            
             if limit == maxCount || limit < maxCount
             {
                 currentCount += 1.0
                 displayLabel.text = "\(currentCount)"
             }
+            
             let newAngleValue = newAngle()
             
             progressView.animate(toAngle: Double(newAngleValue), duration: 0.5, completion: nil)
-            if limit == 10.0
-            {
-                timer?.invalidate()
-            }
+            
             if limit >= 3.0
             {
                 
                 buttonOne.isEnabled = true
                 buttonOne.alpha = 1.0
             }
+            else
+            {
+                buttonOne.isEnabled = false
+                buttonOne.alpha = 0.5
+
+            }
+            
             if limit >= 5.0
             {
                 
                 buttonTwo.isEnabled = true
                 buttonTwo.alpha = 1.0
             }
+            else
+            {
+                buttonTwo.isEnabled = false
+                buttonTwo.alpha = 0.5
+            }
+            
             if limit >= 7.0
             {
                
                 buttonThree.isEnabled = true
                 buttonThree.alpha = 1.0
             }
+            else
+            {
+                buttonThree.isEnabled = false
+                buttonThree.alpha = 0.5
+
+            }
+            
             if limit >= 10.0
             {
                 
                 buttonFour.isEnabled = true
                 buttonFour.alpha = 1.0
             }
+            else
+            {
+                buttonFour.isEnabled = false
+                buttonFour.alpha = 0.5
+            }
         }
         
     }
+    
+    //The AI which is the red block and how he attacks
     func AI()
     {
+        //an array of bools
         var boolAttacks : [Bool] = [true, true, false, false, false]
+        //pulls a random bool from the array
         var random = Int(arc4random_uniform(UInt32(boolAttacks.count)))
+        //picks one of the bools
         var selection = boolAttacks[random]
         
+        //func for attacking
         AIAttack()
+        
+        //runs a loop to see if the arc for random pulled true to see if it gets to attack again
         for i in 0...1
         {
+            
             if selection == true
             {
                 AIAttack()
 
             }
-
+        }
+    }
+    
+    
+    //the func on how the AI Attacks
+    func AIAttack()
+    {
+        
+        //choses one number from the array
+        var attack : [Double] = [3.0, 3.0, 3.0, 3.0, 5.0, 5.0, 5.0, 7.0, 7.0, 10.0]
+        var random = Int(arc4random_uniform(UInt32(attack.count)))
+        var selection = attack[random]
+        
+        //depending on the number it deals out the damage given
+        if selection == 3.0
+        {
+            damageFriend(Damage: 0.05)
+        }
+            
+        else if selection == 5.0
+        {
+            damageFriend(Damage: 0.1)
+            
+        }
+            
+        else if selection == 7.0
+        {
+            damageFriend(Damage: 0.25)
+            
+        }
+            
+        else if selection == 10.0
+        {
+            
+            damageFriend(Damage: 0.4)
         }
         
     }
     
-    func AIAttack()
+    //the func that decreases the progress view
+    func damageFriend(Damage: Double)
     {
-        var attack : [Double] = [3.0,5.0,7.0,10.0]
-        var random = Int(arc4random_uniform(UInt32(attack.count)))
-        var selection = attack[random]
-        if selection == 3.0 {
-            damageFriend(Damage: 0.05)
-        } else if selection == 5.0 {
-            damageFriend(Damage: 0.1)
-            
-        } else if selection == 7.0 {
-            damageFriend(Damage: 0.25)
-            
-        } else if selection == 10.0 {
-            damageFriend(Damage: 0.4)
-            
-        }
-        
-    }
-    func damageFriend(Damage: Double) {
+        //subtracts from the progressview
         currentProgressFriend -= Damage
+        //sets the new values of the progress view
         friendlyHealth.setProgress(Float(currentProgressFriend), animated: true)
     }
     
@@ -173,10 +226,12 @@ class ViewController: UIViewController
         return Int(360 * (currentCount / maxCount))
     }
     
+    
     @IBAction func attackOne(_ sender: Any)
     {
         
         removeNumber(numberToRemove: 3)
+        //sets the attack power of the Friend
         FriendAttackPower += 0.05
     }
     
@@ -185,6 +240,7 @@ class ViewController: UIViewController
         
         removeNumber(numberToRemove: 5)
         FriendAttackPower += 0.1
+        
     }
     
     @IBAction func attackThree(_ sender: Any)
@@ -202,10 +258,13 @@ class ViewController: UIViewController
         
     }
     
-    func damageEnemy(Damage: Double) {
+    //does the same thing as damaging the friend
+    func damageEnemy(Damage: Double)
+    {
         currentProgressEnemy -= Damage
         enemyHealth.setProgress(Float(currentProgressEnemy), animated: true)
     }
+    
     @IBAction func endTurn(_ sender: Any)
     {
         print("Turn Ended")
@@ -223,6 +282,7 @@ class ViewController: UIViewController
     
     func removeNumber(numberToRemove : Double)
     {
+        
         let remove = currentCount - numberToRemove
         
         if remove > 0 || remove == 0
